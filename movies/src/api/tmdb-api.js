b8b4bc12a7dff4342ef722e3186953a7d717c7f3
davinbarron <davinbarron667@gmail.com>
2024-11-14 16:41:13 +0000

    Added getMovieRecommendations

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -134,3 +134,21 @@ export const getMovie = (args) => {
       throw error;
     });
   };
+
+  export const getMovieRecommendations = ({ queryKey }) => {
+    const [, idPart] = queryKey;
+    const { id } = idPart;
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    ).then((response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error;
+    });
+  };  
