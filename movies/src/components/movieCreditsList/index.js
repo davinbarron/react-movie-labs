bf6ebfad8b5c8739379bdca434c2332fa5b8d6a4
davinbarron <davinbarron667@gmail.com>
2024-11-20 17:40:00 +0000

    Removed temp MovieCredit folder
    DRY method, using moviecard after testing moviecredit functionality

--- movies/src/components/movieCreditsList/index.js
+++ movies/src/components/movieCreditsList/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Grid from "@mui/material/Grid2";
-import MovieCredit from "../movieCredit";
+import MovieCard from "../movieCard";
 
 const MovieCreditsList = (props) =>{
   let movieCredits = props.credits.map((m) => (
     <Grid key={m.id} size={{xs: 1, sm: 3, md: 3, lg: 4, xl: 4}} sx={{padding: "10px"}}>
-	  <MovieCredit movie={m}/>
+	  <MovieCard movie={m} action={() => {}}/>
     </Grid>
   ));
   return movieCredits;
